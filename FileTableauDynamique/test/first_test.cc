aa88fd6e2712e61f7e32bf17ef97148d6aa1c19d
Marco André Echeverria <31892221+marcoandre1@users.noreply.github.com>
2020-02-11 04:01:00 -0500

    Update first_test.cc

--- FileTableauDynamique/test/first_test.cc
+++ FileTableauDynamique/test/first_test.cc
@@ -1,9 +1,10 @@
 /**
  * \file FileTesteur.cpp
  * \brief Tests de la classe File en format Google Test
- * \author N/A
+ * \author Ludovic Trottier
+ * \author Mathieu Dumoulin
  * \version 0.3
- * \date N/A
+ * \date Mai 2014
  *
  * Représentation dans un tableau dynamique
  */
@@ -12,27 +13,98 @@
 #include "File.h"
 #include <sstream>
 
-using namespace Laboratoire-Pile-File
+using namespace lab04;
 static const int val1 = 10;
-static const int val1 = 10;
-static const int val1 = 10;
+static const int val2 = 20;
+static const int val3 = 30;
 
-class FileTest: public ::Testing::Test {
+class FileTest: public ::testing::Test {
 public:
-    virtual void SetUp() {
-        file2.enfiler(val1);
-        file2.enfiler(val2);
-        file2.enfiler(val3);
-    }
-    // virtual void TearDown() {}
-    File<int> file1;
-    File<int> file2;
+	virtual void SetUp() {
+		file2.enfiler(val1);
+		file2.enfiler(val2);
+		file2.enfiler(val3);
+	}
+	// virtual void TearDown() {}
+	File<int> file1;
+	File<int> file2;
 };
 
-// ---------------------- (8) -------------------------
-// ----------------------------------------------------
+// ------------------------ (8) ------------------------
+// -----------------------------------------------------
 // void File<T>::enfiler(const T & p_element)
 // T File<T>::defiler()
 TEST_F(FileTest, FileVideOK) {
-    
+	EXPECT_TRUE(file1.estVide());
+	EXPECT_EQ(0, file1.taille());
+}
+
+TEST_F(FileTest, EnfileUnElementOK) {
+	file1.enfiler(val1);
+	EXPECT_EQ(val1, file1.premier());
+	EXPECT_FALSE(file1.estVide());
+	EXPECT_EQ(1, file1.taille());
+	EXPECT_EQ(val1, file1.defiler());
+	EXPECT_TRUE(file1.estVide());
+	EXPECT_EQ(0, file1.taille());
+}
+
+TEST_F(FileTest, EnfileTroisElementsOK) {
+	file1.enfiler(val1);
+	EXPECT_EQ(val1, file1.premier());
+	EXPECT_EQ(val1, file1.dernier());
+	file1.enfiler(val2);
+	EXPECT_EQ(val1, file1.premier());
+	EXPECT_EQ(val2, file1.dernier());
+	file1.enfiler(val3);
+	EXPECT_EQ(val1, file1.premier());
+	EXPECT_EQ(val3, file1.dernier());
+
+	EXPECT_EQ(val1, file1.defiler());
+	EXPECT_EQ(val2, file1.defiler());
+	EXPECT_EQ(val3, file1.defiler());
+	EXPECT_TRUE(file1.estVide());
+}
+
+TEST_F(FileTest, TestCaseOperateurEgal) {
+	File<int> autre = file1;
+
+	EXPECT_EQ(autre.taille(), file1.taille());
+
+	autre = file2;
+
+	EXPECT_EQ(autre.taille(), file2.taille());
+	EXPECT_EQ(autre.premier(), file2.premier());
+	EXPECT_EQ(autre.dernier(), file2.dernier());
+}
+
+TEST_F(FileTest, TestCaseConstructeurCopie) {
+	File<int> autre2(file1);
+
+	EXPECT_EQ(autre2.taille(), file1.taille());
+
+	File<int> autre(file2);
+
+	EXPECT_EQ(autre.taille(), file2.taille());
+	EXPECT_EQ(autre.premier(), file2.premier());
+	EXPECT_EQ(autre.dernier(), file2.dernier());
+}
+
+TEST_F(FileTest, TestCaseOperateurFlotSortie) {
+	std::ostringstream oss;
+	oss << file2;
+
+	EXPECT_EQ("[10,20,30,]", oss.str());
+}
+
+TEST_F(FileTest, operatorCrochetErreur) {
+	file1.enfiler(5);
+	EXPECT_THROW(file1[-1], PreconditionException);
+	EXPECT_THROW(file1[2], PreconditionException);
+}
+
+TEST_F(FileTest, operatorCrochetOk) {
+	EXPECT_EQ(val1, file2[0]);
+	EXPECT_EQ(val2, file2[1]);
+	EXPECT_EQ(val3, file2[2]);
 }
